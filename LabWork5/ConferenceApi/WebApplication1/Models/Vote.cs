b6130951e170b8f31d7b089094790c124e52964d
chupriyanovmm <mmchupriyanov@edu.hse.ru>
2026-03-01 19:58:36 +0500

    change 13

--- LabWork5/ConferenceApi/WebApplication1/Models/Vote.cs
+++ LabWork5/ConferenceApi/WebApplication1/Models/Vote.cs
@@ -2,9 +2,9 @@
 
 public class Vote
 {
-    public int Id { get; set; }
-    public int TalkId { get; set; }
-    public Talk Talk { get; set; } = null!;
+    public int? Id { get; set; }
+    public int? TalkId { get; set; }
+    public Talk? Talk { get; set; } = null!;
     public int? ParticipantId { get; set; }
     public bool Value { get; set; }
 }
